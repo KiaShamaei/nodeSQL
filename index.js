5464907ba3de77dab0cc2ffd695807d04deac002
kiarash.shamaii <kiarash.shamaii@gmail.com>
2021-07-25 08:20:19 +0430

    crud mysql

---
+++ index.js
@@ -0,0 +1,45 @@
+const mysql = require('mysql')
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json())
+
+
+const mysqlConnection = mysql.createConnection({
+	host : "localhost",
+	database : "employeedb",
+	user : "root",
+	password : "KM@852147154"
+})
+mysqlConnection.connect((err)=>{
+	if(!err)
+	console.log("connection successded")
+	else 
+	console.log("error to connect " , JSON.stringify(err,undefined,2))
+})
+
+//get employees
+app.get('/employees' , (req,res)=>{
+	mysqlConnection.query("SELECT * FROM EMPLOYEE" , (err, rows,field)=>{
+		if(!err)
+		res.send(rows)
+		else
+		console.log(err)
+	})
+})
+
+//get an employee 
+app.get('/employees/:id' , (req,res)=>{
+	
+	mysqlConnection.query("SELECT * FROM EMPLOYEE WHERE EMPID= ?",[req.params.id] , (err, rows,field)=>{
+		if(!err)
+		res.send(rows)
+		else
+		console.log(err)
+	})
+})
+
+
+app.listen(3000, ()=>{console.log("express server is runnig successfully!")})
